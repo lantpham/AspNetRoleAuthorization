48a04293aeba2ad5c34beeae872c42ef02ccc91e
lantpham <lannguyenpham@gmail.com>
2017-07-20 12:28:37 -0500

    create default roles in Startup.cs

--- AspNetRoleAuthorization/Controllers/HomeController.cs
+++ AspNetRoleAuthorization/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AspNetRoleAuthorization.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +9,7 @@ using System.Web.Mvc;
 
 namespace AspNetRoleAuthorization.Controllers
 {
+   
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -26,5 +30,10 @@ namespace AspNetRoleAuthorization.Controllers
 
             return View();
         }
+        public ActionResult AddRole()
+        {
+            return View();
+        }
+  
     }
 }
